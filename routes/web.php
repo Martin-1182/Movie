bea41e4751e3eec9b6341e2fea7f0cb53f15fab4
Martin Halaj <info@websystem.sk>
2020-08-17 02:38:20 +0200

    API Usage & HTTP Client

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Http\Controllers\MoviesController;
 use Illuminate\Support\Facades\Route;
 
-Route::view('/', 'index');
-Route::view('/movie', 'show');
+Route::get('/', 'MoviesController@index')->name('movies.index');
+
+Route::get('/movies/{movie}', 'MoviesController@show')->name('movies.show');
+
